09737c8dd4050f288daa2a2ca167bbb0dceed92c
sameer-bagul <sameerbagul2004@gmail.com>
2024-07-20 15:55:06 +0530

    files added

--- Simon Game Challenge Starting Files/script.js
+++
@@ -1,9 +0,0 @@
-var gamePattern = [];
-
-function nextSequence(){
-    var randomNum = Math.floor(Math.random() * 4);
-    var buttonColours = ["red", "blue", "green", "yellow"];
-    var randomChosenColour = buttonColours[randomNum];
-    gamePattern.push(randomChosenColour);
-    // console.log(gamePattern);
-}
