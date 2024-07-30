c1776b662b58b1a39159adf1145dec68d03dae45
sameer <sameerbagul2004@gmail.com>
2024-07-30 07:20:29 +0530

    some more changes

--- section 30 Build your own API/6.0 DIY API/index.js
+++ section 30 Build your own API/6.0 DIY API/index.js
@@ -8,12 +8,41 @@ const masterKey = "4VGP2DN-6EWM4SJ-N6FGRHV-Z3PR3TT";
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //1. GET a random joke
+//  app.get("/{route}", ({requests}}, {responds}) => {
+//    const randomIndex = Math.floor(Math.random() * jokes.length)
+//  });
+
+app.get("/random", (req, res) => {
+  const randomIndex = Math.floor(Math.random() * jokes.length);
+  res.json(jokes[randomIndex]);
+});
 
 //2. GET a specific joke
+app.get("/jokes/:id", (req, res) =>{
+  const id = parseInt(req.params.id);
+  const foundJoke = jokes.find((joke) => joke.id === id);
+  res.json(foundJoke);
+})
 
 //3. GET a jokes by filtering on the joke type
+app.get("/filter", (req, res) => {
+  const type = req.query.type;
+  const filteredActivities = jokes.filter((joke) => joke.jokeType === type);
+  res.json(filteredActivities);
+});
 
 //4. POST a new joke
+// ! this method need a bosy par
+app.post("/jokes", (req, res) => {
+  const newJoke = {
+    id: jokes.length + 1,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  };
+  jokes.push(newJoke);
+  console.log(jokes.slice(-1));
+  res.json(newJoke);
+});
 
 //5. PUT a joke
 
